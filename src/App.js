914f7d5c381a30bf8a439899d21e645b5984a415
shahidmonowarr <shahidmonowar21@gmail.com>
2023-03-01 01:50:43 +0600

    store setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import TodoList from './components/TodoList';
+import store from './redux/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
       <Navbar />
 
@@ -20,6 +23,7 @@ function App() {
         <Footer />
         </div>
     </div>
+    </Provider>
   );
 }
 
